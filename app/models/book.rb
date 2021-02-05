7732026d48d5ac55962345cbf1a9960c7ce06fbd
GoMikami <08g0531mkm@gmail.com>
2021-02-05 12:56:45 +0000

    検索機能fin

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,18 @@ class Book < ApplicationRecord
 	def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
 	end
+	
+	def self.looks(search, words)
+		if search == "perfect_match"
+			@book = Book.where("title LIKE ?", "#{words}")
+		elsif search == "forward_match"
+			@book = Book.where("title LIKE ?", "#{words}%")
+    	elsif search == "backward_match"
+    		@book = Book.where("title LIKE ?", "%#{words}")
+    	else
+    		@book = Book.where("title LIKE ?", "%#{words}%")
+    	end
+	end
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
